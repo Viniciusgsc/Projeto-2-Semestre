982b5f3e54da6e28683f473a24cd4aff7a28d99d
jaummm1 <88463022+jaummm1@users.noreply.github.com>
2022-06-07 20:01:42 -0300

    party projeto base
    base

---
+++ codigoDoProjeto/orm/backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    host: 'localhost',
+    dialect: 'mysql',
+    username: 'root',
+    password: 'toor',
+    database: 'webiiorm',
+    define: {
+        timestamps:true,
+        underscored: true,
+    },
+};
+
