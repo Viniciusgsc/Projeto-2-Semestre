309118cb6f0c2af71a64c033b5372c4862c4be83
jaummm1 <88463022+jaummm1@users.noreply.github.com>
2022-06-08 16:00:24 -0300

    css pagina inicial
    css pagina inicial e tabela de usuarios

--- codigoDoProjeto/orm/frontend/src/components/AreaDados/AreaDados.js
+++ codigoDoProjeto/orm/frontend/src/components/AreaDados/AreaDados.js
@@ -3,11 +3,16 @@ import "./AreaDados.css"
 
 export default function AreaDados() {
   return (
+    <body>
     <div className="areaDados">
-      Saúde Total
-      <img src="/orm/img/capa.jpg"></img>
+      <aside>
+      <h1> Quem somos?</h1>
+      <article>
+      <p>Somos um grupo de Educadores Fisicos das Apaes do estado de São Paulo. Onde ajudamos pessoas com algum tipo de deficiencia sendofisica ou nao. Temos uma ficha de indentificação pessoal de cada um dos nossos pacientes, com informações fornecidas pelo o avaliado ou responsável do mesmo. Esse é um passo crucial para conhecer nossos pacientes e sabermos se esta tendo algum avanço na saude de cada um.</p>
+      </article>
+      </aside>
     </div>
-    
+    </body>
   );
 }
 
--- codigoDoProjeto/orm/frontend/src/components/MenuHorizontal/MenuHorizontal.js
+++ codigoDoProjeto/orm/frontend/src/components/MenuHorizontal/MenuHorizontal.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom"
-
 import "./MenuHorizontal.css"
+import imagem from "../../img/logo.png"
 
 function MenuHorizontal() {
   return (
     <div className="menuHorizontal">
+    <div className="imagem">
+      <img src={imagem}></img></div>  
       <nav className="navMenu">
         <ul className="menus">
           <li> <Link to="/">Home </Link> </li>          
@@ -18,3 +20,6 @@ function MenuHorizontal() {
 }
 
 export default MenuHorizontal;
+
+
+
