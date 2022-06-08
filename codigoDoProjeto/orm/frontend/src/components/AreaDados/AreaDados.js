63bd3c864dabe1acd2ac9509eb57b71c772ec148
jaummm1 <88463022+jaummm1@users.noreply.github.com>
2022-06-08 20:13:29 -0300

    ajustando css
    html e css

--- codigoDoProjeto/orm/frontend/src/components/AreaDados/AreaDados.js
+++ codigoDoProjeto/orm/frontend/src/components/AreaDados/AreaDados.js
@@ -9,9 +9,16 @@ export default function AreaDados() {
       <h1> Quem somos?</h1>
       <article>
       <p>Somos um grupo de Educadores Fisicos das Apaes do estado de São Paulo. Onde ajudamos pessoas com algum tipo de deficiencia sendofisica ou nao. Temos uma ficha de indentificação pessoal de cada um dos nossos pacientes, com informações fornecidas pelo o avaliado ou responsável do mesmo. Esse é um passo crucial para conhecer nossos pacientes e sabermos se esta tendo algum avanço na saude de cada um.</p>
+
+     {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/XozkTgpyxeE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+
       </article>
       </aside>
     </div>
+
+    
+
+
     </body>
   );
 }
