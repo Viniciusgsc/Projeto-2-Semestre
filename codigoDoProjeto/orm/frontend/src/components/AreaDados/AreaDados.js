3830f5af0000e4433f983f0905a399fdd80c10c1
jaummm1 <88463022+jaummm1@users.noreply.github.com>
2022-06-09 22:16:57 -0300

    teste
    kk

--- codigoDoProjeto/orm/frontend/src/components/AreaDados/AreaDados.js
+++ codigoDoProjeto/orm/frontend/src/components/AreaDados/AreaDados.js
@@ -15,11 +15,14 @@ export default function AreaDados() {
       <aside>
         <h1> Quem somos?</h1>
         <article>
-      <p>Somos um grupo de Educadores Fisicos das Apaes do estado de São Paulo. Onde ajudamos pessoas com algum tipo de deficiencia sendofisica ou nao. Temos uma ficha de indentificação pessoal de cada um dos nossos pacientes, com informações fornecidas pelo o avaliado ou responsável do mesmo. Esse é um passo crucial para conhecer nossos pacientes e sabermos se esta tendo algum avanço na saude de cada um.</p>
+      <p>Somos um grupo de Educadores Fisicos das Apaes do estado de São Paulo. Onde ajudamos pessoas com algum tipo de deficiencia sendo fisica ou nao. Temos uma ficha de indentificação pessoal de cada um dos nossos pacientes, com informações fornecidas pelo o avaliado ou responsável do mesmo. Esse é um passo crucial para conhecer nossos pacientes e sabermos se esta tendo algum avanço na saude de cada um.</p>
       </article>
       </aside>
     </div>
-      <div className="video"><iframe width="560" height="315" src="https://www.youtube.com/embed/XozkTgpyxeE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> 
+      <div className="video">
+      <aside>
+        <iframe width="560" height="315" left="300px" src="https://www.youtube.com/embed/XozkTgpyxeE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ></iframe> 
+        </aside>
       </div>
   </body>
 </html>
